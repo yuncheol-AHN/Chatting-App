0088d2f47210ce7e83419c66214db0b1dacd2b95
안윤철 <anchul@hanmail.net>
2022-11-13 19:52:27 +0900

    Feat : "UI 구성"

---
+++ Chatting-App/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  Chatting-App
+//
+//  Created by 안윤철 on 2022/11/13.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "LoginToChat", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
